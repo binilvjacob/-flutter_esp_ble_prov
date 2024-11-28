ce4207e3d272e7ca696a6231c5bb2fc844415967
binilvjacob <itsmebinilvjacob@gmail.com>
2024-11-28 17:13:26 +0530

    updated

--- android/src/main/kotlin/how/virc/flutter_esp_ble_prov/FlutterEspBleProvPlugin.kt
+++ android/src/main/kotlin/how/virc/flutter_esp_ble_prov/FlutterEspBleProvPlugin.kt
@@ -296,22 +296,11 @@ class BleScanManager(boss: Boss) : ActionManager(boss) {
     boss.d("searchBleEspDevices: start")
     val prefix = ctx.arg("prefix") ?: return
 
-if (activity == null) {
-    Log.e("Bluetooth", "Activity context is null")
-   // ctx.result.error("INVALID_CONTEXT", "Activity context is required for enabling Bluetooth", null)
-    return
-}
-
-    //  if (!isBluetoothEnabled()) {
-    //     Log.e("Bluetooth", "Bluetooth is OFF or not supported")
-    //     // Prompt the user to enable Bluetooth
-    //     val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-    //     activity.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
-
-    //     // Wait for user to enable Bluetooth before proceeding
-    //     ctx.result.error("BLUETOOTH_DISABLED", "Prompted user to enable Bluetooth", null)
-    //     return
-    // }
+     if (!isBluetoothEnabled()) {
+            Log.e("Bluetooth", "Bluetooth is OFF or not supported")
+            ctx.result.error("BLUETOOTH_DISABLED", "Bluetooth is not enabled", null)
+            return
+        }
 
     boss.espManager.searchBleEspDevices(prefix, object : BleScanListener {
       override fun scanStartFailed() {
